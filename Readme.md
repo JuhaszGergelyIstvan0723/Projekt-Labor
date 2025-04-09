ef2f317ee8ab3701e1ecb567106a8b05a0e80525
JuhaszGergelyIstvan0723 <juhasz.gergely-2022@keri.mako.hu>
2025-04-09 23:41:01 +0200

    Add files via upload
    0409 2333 -Utolsó símítások

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Labor Projekt
+=====
+# Csapat
+
+| Név              | "Szakterület"           
+|------------------|----------------------|
+| Tóth Dzsenifer   |  Frontend            |
+| Juhász Gergely  |  Backend és frontend  |
+
+# Felhasznált oldalak
+- Bootstap
+- W3School
+- GitHub
+- Trello
+- Figma
--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
+let examBasic = document.getElementById('exam');
+let info = document.getElementById("info");
+
+window.addEventListener("DOMContentLoaded", infoLink)
+
+function infoLink() {
+  info.setAttribute("href", `arak.html#${examBasic.value}`);
+}
+
+
+examBasic.addEventListener("change", () => {
+  info.setAttribute("href", `arak.html#${examBasic.value}`);
+})
+
+
+
+
+
 function showSelected() {
-    let name = document.getElementById('name').value.trim();
-    let email = document.getElementById('email').value.trim();
-    let taj = document.getElementById('taj').value.trim();
-    let date = document.getElementById('date').value;
-    let hour = document.getElementById('hour').value;
-    let minute = document.getElementById('minute').value;
-    let exam = document.getElementById('exam').value;
-    let btnAnim= document.getElementById('forAnim');
-    btnAnim.classList.remove("btn-animation");
+  let name = document.getElementById('name').value.trim();
+  let email = document.getElementById('email').value.trim();
+  let taj = document.getElementById('taj').value.trim();
+  let date = document.getElementById('date').value;
+  let hour = document.getElementById('hour').value;
+  let minute = document.getElementById('minute').value;
+  let exam = document.getElementById('exam').value;
+  let btnAnim = document.getElementById('forAnim');
 
-    if (!name || !email || !taj || !date) {
-      document.getElementById('result').textContent = "Kérlek, töltsd ki az összes mezőt!";
-      return;
-    }else{
-        let selectedDateTime = `${date} ${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`;
+  btnAnim.classList.remove("btn-animation");
 
-        btnAnim.classList.add(".btn-animation");
+
+  if (!name || !email || !taj || !date) {
+    document.getElementById('result').textContent = "Kérlek, töltsd ki az összes mezőt!";
+    return;
+  } else {
+    let selectedDateTime = `${date} ${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`;
+    btnAnim.classList.add(".btn-animation");
 
     document.getElementById('result').innerHTML = `
       <p><strong>Név:</strong> ${name}</p>
@@ -28,21 +47,21 @@ function showSelected() {
       <br>
       <p id="countdown"></p>
     `;
-        
-    }
+
+  }
 }
 
-    function backMainPage(delayMs = 5000) {
-        let countdownElem = document.getElementById("countdown");
-        let remainingSec = Math.floor(delayMs / 1000);
-    
-        let interval = setInterval(() => {
-          remainingSec--;
-          countdownElem.textContent = `${remainingSec} ms múlva visszairányítás a főoldalra...`;
-    
-          if (remainingSec <= 0) {
-            clearInterval(interval);
-            window.location.href = "../index.html"; 
-          }
-        }, 1000);
-      }
+function backMainPage(delayMs = 5000) {
+  let countdownElem = document.getElementById("countdown");
+  let remainingSec = Math.floor(delayMs / 1000);
+
+  let interval = setInterval(() => {
+    remainingSec--;
+    countdownElem.textContent = `${remainingSec} ms múlva visszairányítás a főoldalra...`;
+
+    if (remainingSec <= 0) {
+      clearInterval(interval);
+      window.location.href = "../index.html";
+    }
+  }, 1000);
+}
